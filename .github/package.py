d31871b8f6ef35c83d7b6efa1552500e04ac5b57
stilobique <stilobique@stilobique.com>
2022-02-01 18:58:32 +0100

    Add package setup test

---
+++ .github/package.py
@@ -0,0 +1,13 @@
+import glob
+import os
+
+
+def get_folder_name():
+    addon = glob.glob(os.getcwd() + "/*/__init__.py", recursive=True)
+
+    return os.path.basename(os.path.dirname(addon[0]))
+
+
+if __name__ == "__main__":
+    name = get_folder_name()
+    print(f'Show this value : {name}')
