cff2f0ca39ae2af87cec4bc1fdd386d84af4802c
Aurelien Vaillant <aurelienvlt@free.fr>
2022-02-02 10:57:20 +0100

    Update debug script

--- .github/package.py
+++ .github/package.py
@@ -10,7 +10,10 @@ def get_folder_name():
 
 if __name__ == "__main__":
     name = get_folder_name()
+    print(f'Python : The folder name find are "{name}"')
 
-    with open('$GITHUB_ENV', 'a') as f:
+    with open('$GITHUB_ENV', 'w') as f:
         f.write(f'APP_NAME={name}')
         f.write(f'NAME_PACKAGE={name}.zip')
+        print('Update all env. variable')
+        f.close()
